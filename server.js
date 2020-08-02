0cf1c38e7ee61cc301afcd97ee95b1b10524b8ba
Arpita K Sau <arpitakaran@gmail.com>
2020-08-01 19:34:33 -0500

    added-add-role-and-add-emp-function

--- server.js
+++ server.js
@@ -50,12 +50,6 @@ function renderAction() {
             case "View Employees":
                 viewEmp();
                 break;
-            case "View Employees By Manager":
-                viewEmpByManager();
-                break;
-            case "View Budget of a Department":
-                viewBudgets();
-                break;
             case "Update Employee Roles":
                 updateEmpRoles();
                 break;
@@ -108,25 +102,116 @@ function addDept(){
 
 };
 function addRoles(){
+    inquirer.prompt([
+        {
+            name: "title",
+            massage: "Enter Job Title:",
+            type: "input",
+            validate : validateInput
+    },
+    {
+        name: "salary",
+            massage: "Enter Salary:",
+            type: "input",
+            validate : validateInput
+
+    },
+    {
+    name: "department_id",
+            massage: "Enter department ID:",
+            type: "input",
+            validate : validateInput
+    }
+
+]).then(answer => {
+    connection.query("INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)",
+    [answer.title, answer.salary, answer.department_id,],
+    function(err){
+        if (err)throw err;
+        renderAction();
+    });
+})
 
 
 };
 function addEmp(){
+    connection.query(
+        `select r.title Title, r.salary Salary, d.name Department, r.id RoleID
+         from role r
+         left join department d on (d.id = r.department_id)
+         order by r.title,d.name`,
+    function(err, results) {
+        let roleOption = ["No Role"];
+        if (err) throw err;
+        results.forEach(element => {
+            roleOption.push(`${element.Title} - ${element.Salary} - ${element.Department} - ${element.RoleID}`);
+        });
+        connection.query(
+            `select e.first_name FirstName,e.last_name LastName,d.name Department,r.title Title,e.id EmployeeID,concat(m.first_name," ",m.last_name) Manager
+             from employee e
+             left join role r on (e.role_id = r.id)
+             left join department d on (r.department_id = d.id)
+             left join employee m on (e.manager_id = m.id)
+             order by e.first_name,e.last_name,d.name,r.title`,
+        function(err2,results2) {
+            let managerArr = ["No Manager"];
+            if (err2) throw err2;
+            results2.forEach(element => {
+                managerArr.push(`${element.FirstName} ${element.LastName} - ${element.Title} - ${element.Department} - ${element.EmployeeID}`);
+            });
+            inquirer.prompt([
+                {
+                    name: "first_name",
+                    type: "input",
+                    message: "Enter first name:",
+                    validate: validateInput
+                },
+                {
+                    name: "last_name",
+                    type: "input",
+                    message: "Enter last name:",
+                    validate: validateInput
+                },
+                {
+                    name: "role_id",
+                    type: "list",
+                    message: "Choose role (Title - Salary - Department - RoleID):",
+                    choices: roleOption
+                },
+                {
+                    name: "manager_id",
+                    type: "list",
+                    message: "Choose manager (Name - Title - Department - EmployeeID):",
+                    choices: managerArr
+                }
+            ]).then(answers => {
+                let roleId = (answers.role_id === "No Role" ) ? null : parseInt(answers.role_id.split(' - ').pop());
+                let managerId = (answers.manager_id === "No Manager" ) ? null : parseInt(answers.manager_id.split(' - ').pop());
+                connection.query("insert into employee (first_name,last_name,role_id,manager_id) values (?,?,?,?)",
+                [answers.first_name,answers.last_name,roleId,managerId],
+                function(err) {
+                    if (err) throw err;
+                    renderAction();
+                }
+                );
+            });
+        });
+    });
+}
 
-};
 function viewDept(){
     connection.query(`SELECT name, id FROM department;`, (err, results)=>{
         if(err)throw err;
         console.table(results);
         renderAction()
-    });
+    })
 }
 function viewRoles(){
     connection.query(`SELECT * FROM role;`,(err, results)=>{
         if(err)throw err;
         console.table(results);
         renderAction()
-    });
+    })
 
 }
 function viewEmp(){
@@ -140,16 +225,12 @@ function viewEmp(){
         if(err)throw err;
         console.table(results);
         renderAction()
-    });
+    })
 
 
 }
-function viewEmpByManager(){
 
-}
-function viewBudgets(){
 
-}
 function updateEmpRoles(){
 
 }
